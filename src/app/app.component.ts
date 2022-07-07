a942f5160f564a9d31aff906aaf214bc4854a343
carolsouza <caah.souza@live.com>
2022-07-07 17:12:50 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'carolinesp';
+
+  userData = {
+    email: "carolinesp@email",
+    age: 25,
+    city: "Bauru"
+  }
+
   title = 'AngularProject';
 }
